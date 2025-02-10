9f3ed5b8d7335ac057bcc1a16a2440ae321e958b
jlansk <155968035+jlansk@users.noreply.github.com>
2025-02-10 16:45:09 +0000

    Add files via upload

---
+++ cmm_environment.m
@@ -0,0 +1,16 @@
+function E = cmm_environment()
+
+%% Set up environment
+E.Fspm = '/imaging/local/software/spm_cbu_svn/releases/spm12_fil_r7771/';
+addpath(E.Fspm);
+spm('defaults', 'EEG');
+
+E.scr='/imaging/rowe/users/jl01/meg/dcm_cmm/scripts/to_publish/';
+addpath(genpath(E.scr));
+
+E.raw='/imaging/rowe/users/jl01/meg/dcm_cmc/meg_data/';
+
+E.anaB =   '/imaging/rowe/users/jl01/meg/dcm_cmm/reps0-5/BL_Feb23/full/noA3';
+E.anaL =  '/imaging/rowe/users/jl01/meg/dcm_cmm/reps0-5/AF_Feb23/full/noA3';
+
+end
